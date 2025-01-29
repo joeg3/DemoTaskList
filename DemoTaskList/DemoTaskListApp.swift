a18ab843ab82a6403b975b1c6fd7c90612cec0f1
Joe <jgliniecki@gmail.com>
2025-01-29 10:37:28 -0600

    Initial Commit

---
+++ DemoTaskList/DemoTaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTaskListApp.swift
+//  DemoTaskList
+//
+//  Created by Joseph Gliniecki on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoTaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
